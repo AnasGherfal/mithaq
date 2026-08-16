46464654b31105cc0f0727834005d524a3bdd097
AnasGherfal <79544599+AnasGherfal@users.noreply.github.com>
2026-08-16 19:09:31 +0300

    Add Libya and Diaspora page

---
+++ src/app/(localized)/[locale]/libya-diaspora/page.tsx
@@ -0,0 +1,5 @@
+import { TranslatedTrustPage } from "@/components/public/translated-trust-page";
+
+export default function LibyaDiasporaPage() {
+  return <TranslatedTrustPage namespace="Diaspora" />;
+}
